359c9e18f634b1cde61755d0c3d04f6776850ab8
vonsogt <35516476+vonsogt@users.noreply.github.com>
2020-03-27 16:43:41 +0700

    add login menu
    add login menu in frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -66,6 +66,7 @@
                     <li><a href="#faq-page">FAQ</a></li>
                     <li><a href="#blog-page">Blog</a></li>
                     <li><a href="#contact-page">Contact</a></li>
+                    <li><a href="/admin">Login</a></li>
                 </ul>
             </nav>
         </div>
